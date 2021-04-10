395a305f68180688cca150579ca38c2d9f50ca52
prafful0026 <prafful0026@gmail.com>
2021-04-11 01:05:02 +0530

    added appoint lawyer page

--- frontend/src/pages/AppointPage/appoint-page.jsx
+++ frontend/src/pages/AppointPage/appoint-page.jsx
@@ -6,60 +6,113 @@ import Loader from "../../components/Loader/Loader.jsx";
 import FormContainer from "../../components/FormContainer/FormContainer.jsx";
 import { listLawyerDetails } from "../../actions/lawyerActions.js";
 const AppointPage = ({ location, history, match }) => {
-  const [desc, setDesc] = useState("");
-  const [evidence, setEvidence] = useState("");
+  const [caseDescription, setCaseDescription] = useState("");
+  const [caseCategory, setCaseCategory] = useState("");
+  const [caseId, setCaseId] = useState("");
+  const [email, setEmail] = useState("");
+  const [contactNo, setContactNo] = useState("");
   const [errorMessage, setErrorMessage] = useState(null);
   const dispatch = useDispatch();
   const userLogin = useSelector((state) => state.userLogin);
+
   const { loading, error, userInfo } = userLogin;
 
   const lawyerDetails = useSelector((state) => state.lawyerDetails);
-  const {lawyer} = lawyerDetails;
-  
-  useEffect(() => {
-    if(!userInfo)
-    history.push('/userLogin')
-    dispatch(listLawyerDetails(match.params.id));    
-    if(lawyer.name&&!lawyer.isAvailable)
-    setErrorMessage("LAWYER NOT AVAILABLE!!")
-    else if(lawyer._id===1)
-    setErrorMessage("LAWYER NOT FOUND!!")
+  const { lawyer, error: lawyerError } = lawyerDetails;
 
-  }, [dispatch, match, history,lawyer._id]);
+  useEffect(() => {
+    if (!userInfo) history.push("/userLogin");
+    dispatch(listLawyerDetails(match.params.id));
+    if (lawyer.name && !lawyer.isAvailable)
+      setErrorMessage("LAWYER NOT AVAILABLE!!");
+  }, [dispatch, match, history, lawyer.isAvailable]);
+
+  useEffect(() => {
+    if (lawyerError) setErrorMessage("LAWYER NOT FOUND!!");
+  }, [lawyerError]);
 
   const submitHandler = (e) => {
-    e.preventDefault();
+    let userId=userInfo._id
+    let lawyerId=lawyer._id
+
+    e.preventDefault()
+    const Case={caseId,caseCategory,caseDescription,email,lawyerId,userId}
+    console.log(Case)
   };
   return (
     <FormContainer>
       <h1>appoint</h1>
-      {error && <Message variant="danger">{error}</Message>}
-      {errorMessage && <Message variant="danger">{errorMessage}</Message>}
+      {error && <Message variant='danger'>{error}</Message>}
+      {errorMessage && <Message variant='danger'>{errorMessage}</Message>}
       {loading && <Loader></Loader>}
       <Form onSubmit={submitHandler}>
-        <Form.Group controlId="desc">
-          <Form.Label>CASE DESC</Form.Label>
+
+      <Form.Group controlId='caseId'>
+          <Form.Label>Case ID</Form.Label>
           <Form.Control
-            type="text"
             disabled={!lawyer || !lawyer.isAvailable}
-            placeholder="Enter Description"
-            value={desc}
+            type='text'
+            placeholder='Enter caseId'
+            value={caseId}
             onChange={(e) => {
-              setDesc(e.target.value);
+              setCaseId(e.target.value);
+              setErrorMessage(null);
+            }}
+          ></Form.Control>
+        </Form.Group>
+         
+        <Form.Group controlId='category'>
+          <Form.Label>Category</Form.Label>
+          <Form.Control
+            disabled={!lawyer || !lawyer.isAvailable}
+            type='text'
+            placeholder='Enter Category'
+            value={caseCategory}
+            onChange={(e) => {
+              setCaseCategory(e.target.value);
               setErrorMessage(null);
             }}
           ></Form.Control>
         </Form.Group>
 
-        <Form.Group controlId="evidence">
-          <Form.Label>EVIDENCE</Form.Label>
+        <Form.Group controlId='description'>
+          <Form.Label>CASE DESC</Form.Label>
+          <Form.Control
+            as='textarea'
+            rows={3}
+            disabled={!lawyer || !lawyer.isAvailable}
+            placeholder='Enter Description'
+            value={caseDescription}
+            onChange={(e) => {
+              setCaseDescription(e.target.value);
+              setErrorMessage(null);
+            }}
+          ></Form.Control>
+        </Form.Group>
+
+        <Form.Group controlId='email'>
+          <Form.Label>Your Email</Form.Label>
           <Form.Control
             disabled={!lawyer || !lawyer.isAvailable}
-            type="text"
-            placeholder="Enter Evidence"
-            value={evidence}
+            type='email'
+            placeholder='Enter Email'
+            value={email}
             onChange={(e) => {
-              setEvidence(e.target.value);
+              setEmail(e.target.value);
+              setErrorMessage(null);
+            }}
+          ></Form.Control>
+        </Form.Group>
+
+        <Form.Group controlId='contactNo'>
+          <Form.Label>Your Contact No.</Form.Label>
+          <Form.Control
+            disabled={!lawyer || !lawyer.isAvailable}
+            type='tel'
+            placeholder='Enter Contact Number'
+            value={contactNo}
+            onChange={(e) => {
+              setContactNo(e.target.value);
               setErrorMessage(null);
             }}
           ></Form.Control>
@@ -67,8 +120,8 @@ const AppointPage = ({ location, history, match }) => {
 
         <Button
           disabled={!lawyer || !lawyer.isAvailable}
-          type="submit"
-          variant="primary"
+          type='submit'
+          variant='primary'
         >
           Appoint
         </Button>
